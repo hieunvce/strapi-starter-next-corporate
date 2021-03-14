1dadc0144528abc00ce610597e4c3827ff1f6d48
Hieu Nguyen <hieunv.ce@gmail.com>
2021-03-14 15:27:18 +0700

    feat: Add docker-compose to start local dev env
    - Add docker-compose.yaml to start local dev environment
    - Add Dockerfile for backend service
    - Add Dockerfile for frontend service
    - Add .env file to storage environment variables

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '56cb938b53979b54487a48fdadbee448'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3f5ee33-1631-4fb8-9b1a-893779a24939'
+};
